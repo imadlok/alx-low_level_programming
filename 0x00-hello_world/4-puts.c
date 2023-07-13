209eafd582f7a1f9cbe994f7939d098bf5c21c50
imadlok <imadtony99@gmail.com>
2023-07-13 15:31:06 -0700

    add file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - point input
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
